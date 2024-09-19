a46f2473a2d7d631e054d37b31ccc36062ab9b6f
khalidkarimkqr <khalid13878@hotmail.com>
2024-09-19 01:36:35 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a83b89b9-99c4-4ed2-a739-09d3818ca80a';
+export const MODAL_CLOSE_SEC = 2.5;
